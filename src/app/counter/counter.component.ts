446a9eb7946bd3d893e3412fa25b614c87ab444b
Murari Thakur <itsmeanimatormt@gmail.com>
2023-02-23 12:04:51 +0530

    Day-1-My-First-Angular-App

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  count=0;
+  increment(){
+    return this.count++;
+  }
+  decrement(){
+    return this.count--;
+  }
+}
